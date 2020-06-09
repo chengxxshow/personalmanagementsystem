d4939b151a7998087272189f53d2ff6803cb7ee4
chengxxshow <563255034@qq.com>
2020-06-09 13:03:53 +0800

    update1

--- src/main/java/com/neuedu/personalmanagementsystem/controller/UserController.java
+++ src/main/java/com/neuedu/personalmanagementsystem/controller/UserController.java
@@ -4,19 +4,22 @@ import com.neuedu.personalmanagementsystem.entity.User;
 import com.neuedu.personalmanagementsystem.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
+@RequestMapping("/user")
 public class UserController {
     @Autowired
    private IUserService  userService;
 
     @RequestMapping("/login")
     @ResponseBody
-    public String login(String username, String password){
+    public String login(@RequestBody User lUser){
         System.out.println("111"+userService);
-        User user=userService.getUserBynameandpsw(username,password);
+        System.out.println(lUser+"-------");
+        User user=userService.getUserBynameandpsw(lUser.getUsername(),lUser.getPassword());
         if(user!=null){
             return "login success";
         }else{
